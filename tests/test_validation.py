5da4729fdac658c2577a77e3e2ce54e31fd3a492
Nicolas Grobelny <miki4920@gmail.com>
2021-09-24 17:09:26 +0100

    Removed Pillow and BytesIO from dependencies

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,8 +1,10 @@
+import pytest
 import random
 import string
 
 from config import CONFIG
-from validation import validate_name, validate_extension, validate_dimensions, validate_battlemap
+from errors import ValidationError
+from validation import validate_name, validate_extension, validate_dimensions, validate_square_dimensions, validate_tags, validate_battlemap
 
 
 def get_valid_name():
@@ -53,16 +55,49 @@ def test_validate_dimensions():
     assert not validate_dimensions(invalid, invalid)
 
 
+def get_valid_square_dimension():
+    return random.choice([str(random.randint(1, 999)), None])
+
+
+def get_invalid_square_dimension():
+    return "".join([random.choice(string.ascii_lowercase) for _ in range(3)])
+
+
+def test_validate_square_dimensions():
+    valid = get_valid_square_dimension()
+    invalid = get_invalid_square_dimension()
+    assert validate_square_dimensions(valid, valid)
+    assert not validate_square_dimensions(invalid, invalid)
+
+
+def get_valid_tags():
+    return ",".join([random.choice(string.ascii_lowercase) for _ in range(CONFIG.MAXIMUM_NAME_LENGTH // 2)])
+
+
+def get_invalid_tags():
+    return "_".join([random.choice(string.ascii_lowercase) for _ in range(CONFIG.MAXIMUM_NAME_LENGTH // 2)])
+
+
+def test_validate_tags():
+    valid = get_valid_tags()
+    invalid = get_invalid_tags()
+    assert validate_tags(valid)
+    assert not validate_tags(invalid)
+
+
 def test_validate_battlemap():
     valid = {"name": get_valid_name(),
              "extension": get_valid_extension(),
              "width": get_valid_dimension(),
-             "height": get_valid_dimension()}
+             "height": get_valid_dimension(),
+             "tags": get_valid_tags()}
 
     invalid = {"name": get_invalid_name(),
                "extension": get_invalid_extension(),
                "width": get_invalid_dimension(),
-               "height": get_invalid_dimension()}
+               "height": get_invalid_dimension(),
+               "tags": get_invalid_tags()}
 
-    assert validate_battlemap(valid)
-    assert not validate_battlemap(invalid)
+    assert validate_battlemap(valid) is None
+    with pytest.raises(ValidationError):
+        validate_battlemap(invalid)
