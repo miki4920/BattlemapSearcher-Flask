1e603ad2de15bd845beeaf8f8c0c40a42e1a829e
Nicolas Grobelny <miki4920@gmail.com>
2021-09-24 17:03:47 +0100

    Implemented error messages in the form of a dictionary. Cut down some redundant errors.

--- errors.py
+++ errors.py
@@ -2,64 +2,84 @@ from config import CONFIG
 
 
 class ValidationError(Exception):
-    pass
+    def __init__(self):
+        self.message = None
+        self.error_code = 400
 
 
 class NameInvalid(ValidationError):
     def __init__(self, name):
-        self.message = f"""01:Map Name is invalid. It can only contain alphanumerical characters and '_'. 
-        Its length must also be within this range:{CONFIG.MINIMUM_NAME_LENGTH},{CONFIG.MAXIMUM_NAME_LENGTH}:{name}"""
+        super().__init__()
+        self.message = {"error_code": "01",
+                        "map_name": name,
+                        "invalid": name,
+                        "message": f"""Map Name is invalid. It can only contain alphanumerical characters and '_'. 
+        Its length must also be within the range:{CONFIG.MINIMUM_NAME_LENGTH},{CONFIG.MAXIMUM_NAME_LENGTH}"""}
 
 
 class ExtensionInvalid(ValidationError):
-    def __init__(self, extension):
-        self.message = f"""02:Map Extension is invalid. 
-        Currently accepted extensions are:{','.join(CONFIG.IMAGE_EXTENSIONS)}):{extension}"""
+    def __init__(self, name, extension):
+        super().__init__()
+        self.message = {"error_code": "02",
+                        "map_name": name,
+                        "invalid": extension,
+                        "message": f"""Map Extension is invalid. 
+        Currently accepted extensions are:{','.join(CONFIG.IMAGE_EXTENSIONS)}"""}
 
 
 class DimensionsInvalid(ValidationError):
-    def __init__(self, width, height):
-        self.message = f"""03:Map Dimensions are invalid. Currently acceptable minimum dimension is:
-        {CONFIG.MINIMUM_IMAGE_DIMENSION}:{width},{height}"""
+    def __init__(self, name, width, height):
+        super().__init__()
+        self.message = {"error_code": "03",
+                        "map_name": name,
+                        "invalid": f"{width},{height}",
+                        "message": f"""Map Dimensions are invalid. Currently acceptable minimum dimension is:
+        {CONFIG.MINIMUM_IMAGE_DIMENSION}"""}
+
+
+class SquareDimensionsInvalid(ValidationError):
+    def __init__(self, name, square_width, square_height):
+        super().__init__()
+        self.message = {"error_code": "04",
+                        "map_name": name,
+                        "invalid": f"{square_width},{square_height}",
+                        "message": f"""Map Square Dimensions are invalid. Both dimensions need to either consist of up
+                         to 3 digits or be Nones."""}
 
 
 class ImageInvalid(ValidationError):
-    def __init__(self, image):
+    def __init__(self, name, image):
         image_size = len(image)
-        self.message = f"""03:Map Image is invalid. Currently accepted size range is:
-        {CONFIG.MINIMUM_IMAGE_SIZE_IN_BYTES},{CONFIG.MAXIMUM_IMAGE_SIZE_IN_BYTES}:{image_size}"""
+        super().__init__()
+        self.message = {"error_code": "05",
+                        "map_name": name,
+                        "invalid": image_size,
+                        "message": f""":Map Image Size is invalid. Currently accepted size range is:
+        {CONFIG.MINIMUM_IMAGE_SIZE_IN_BYTES},{CONFIG.MAXIMUM_IMAGE_SIZE_IN_BYTES}"""}
 
 
 class HashBlacklisted(ValidationError):
     def __init__(self, name, battlemap_hash):
-        self.message = f"04:Map Hash is blacklisted:{name}:{battlemap_hash}"
+        super().__init__()
+        self.message = {"error_code": "06",
+                        "map_name": name,
+                        "invalid": battlemap_hash,
+                        "message": f"""Map Hash is blacklisted"""}
 
 
 class HashRepeated(ValidationError):
     def __init__(self, name, battlemap_hash):
-        self.message = f"05:Map Hash is already in the database:{name}:{battlemap_hash}"
+        super().__init__()
+        self.message = {"error_code": "07",
+                        "map_name": name,
+                        "invalid": battlemap_hash,
+                        "message": f"""Map Hash is already in the database"""}
 
 
-class SquareDimensionsNotAccepted(ValidationError):
-    def __init__(self, square_width, square_height):
-        self.message = f"07:Map Picture is missing one of the square dimensions:{square_width},{square_height}"
-
-
-class SquareDimensionsNotInRange(ValidationError):
-    def __init__(self, square_width, square_height):
-        self.message = f"08:One of the square dimensions is not an Integer or Null:{square_width},{square_height}"
-
-
-class UploaderNotAlphanumerical(ValidationError):
-    def __init__(self, uploader):
-        self.message = f"09:Map Uploader contains non-alphanumerical characters:{uploader}"
-
-
-class UploaderNotInRange(ValidationError):
-    def __init__(self, uploader):
-        self.message = f"10:Map Uploader has wrong length:{uploader}"
-
-
-class TagsNotAccepted(ValidationError):
-    def __init__(self, tags):
-        self.message = f"11:Map Tags are in a wrong format:{tags}"
+class TagsInvalid(ValidationError):
+    def __init__(self, name, tags):
+        super().__init__()
+        self.message = {"error_code": "08",
+                        "map_name": name,
+                        "invalid": tags,
+                        "message": f"""Map Tags are invalid."""}
