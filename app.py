bf1d0f7763096a0d65f33df1f912160460da8506
Nicolas Grobelny <miki4920@gmail.com>
2021-09-27 14:14:23 +0100

    Implemented SQL database

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
-from flask import Flask, render_template, url_for, request, make_response
+import os
+import io
+
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
+from PIL import Image
 
 from config import CONFIG
 from errors import ValidationError
+from getting import *
 from validation import validate_battlemap
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f'mysql://{os.getenv("USERNAME")}:{os.getenv("PASSWORD")}@{os.getenv("IP")}/flask'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 tags_table = db.Table('tags',
-                db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-                db.Column('map_id', db.Integer, db.ForeignKey('map.id'), primary_key=True)
-                )
+                      db.Column('tag_id', db.String(CONFIG.MAXIMUM_NAME_LENGTH), db.ForeignKey('tag.id'),
+                                primary_key=True),
+                      db.Column('map_id', db.Integer, db.ForeignKey('map.id'), primary_key=True)
+                      )
 
 
 class Tag(db.Model):
@@ -26,11 +33,11 @@ class Map(db.Model):
     extension = db.Column(db.String(3))
     hash = db.Column(db.String(16))
     path = db.Column(db.String(CONFIG.MAXIMUM_NAME_LENGTH * 2))
+    thumbnail_path = db.Column(db.String(CONFIG.MAXIMUM_NAME_LENGTH * 2))
     width = db.Column(db.INTEGER)
     height = db.Column(db.INTEGER)
     square_width = db.Column(db.INTEGER, nullable=True)
     square_height = db.Column(db.INTEGER, nullable=True)
-    uploader = db.Column(db.String(CONFIG.MAXIMUM_NAME_LENGTH))
     tags = db.relationship("Tag", secondary=tags_table,
                            backref=db.backref("maps"))
 
@@ -46,10 +53,30 @@ def main():
     return render_template("main.html")
 
 
-if __name__ == '__main__':
-    app.run()
-    app.add_url_rule("/favicon.ico",
-                     redirect_to=url_for("static", filename="icons/favicon.ico"))
+def save_file(data, file):
+    path = get_path(data)
+    stream = io.BytesIO(file.stream.read())
+    file.save(path)
+
+    thumbnail_path = get_thumbnail_path(data)
+    extension = get_extension(data).upper()
+
+    with Image.open(stream) as thumbnail:
+        thumbnail = thumbnail.resize((CONFIG.THUMBNAIL_SIZE, CONFIG.THUMBNAIL_SIZE), Image.ANTIALIAS)
+        thumbnail.save(thumbnail_path, "JPEG" if extension == "JPG" else extension)
+
+
+def create_map(data):
+    name = get_name(data)
+    extension = get_extension(data)
+    battlemap_hash = get_hash(data)
+    path = get_path(data)
+    thumbnail_path = get_thumbnail_path(data)
+    width, height = get_dimensions(data)
+    square_width, square_height = get_square_dimensions(data)
+    battlemap = Map(name=name, extension=extension, hash=battlemap_hash, path=path, thumbnail_path=thumbnail_path,
+                    width=width, height=height, square_width=square_width, square_height=square_height)
+    return battlemap
 
 
 @app.post("/maps")
@@ -58,8 +85,26 @@ def post_maps():
     file = request.files["image"]
     try:
         validate_battlemap(data)
+        save_file(data, file)
+        battlemap = create_map(data)
+        if data.get("tags") is not None:
+            for tag in data.get("tags").split(","):
+                query_tag = Tag.query.filter_by(id=tag).first()
+                if query_tag is None:
+                    query_tag = Tag(id=tag)
+                battlemap.tags.append(query_tag)
+        db.session.add(battlemap)
+        db.session.commit()
         return "Valid", 200
     except ValidationError as e:
         return e.message, e.error_code
     except Exception as e:
         raise e
+
+
+if __name__ == "__main__":
+    db.create_all()
+    db.session.commit()
+    app.run()
+    app.add_url_rule("/favicon.ico",
+                     redirect_to=url_for("static", filename="icons/favicon.ico"))
