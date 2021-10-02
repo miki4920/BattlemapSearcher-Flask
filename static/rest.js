2f96ee357a3777c5ba340d40f133af7b94b538d1
Nicolas Grobelny <miki4920@gmail.com>
2021-10-02 17:32:35 +0100

    Fixed typos in css and javascript

--- static/rest.js
+++ static/rest.js
@@ -14,8 +14,6 @@ function delete_map(url, map_id) {
     let base_url = window.location.origin;
     let xhttp = new XMLHttpRequest();
     xhttp.open("DELETE", base_url + url);
-    xhttp.onreadystatechange = function() {
-        document.getElementById(map_id).remove();
-    }
+    document.getElementById(map_id).remove();
     xhttp.send()
 }
