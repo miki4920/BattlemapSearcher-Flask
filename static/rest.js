c5f9aebe1acc74adcb7693e4c01123c3e3874b59
Nicolas Grobelny <miki4920@gmail.com>
2021-10-06 21:29:24 +0100

    Added a timeout to foundry button

--- static/rest.js
+++ static/rest.js
@@ -1,12 +1,16 @@
-function get_map(url, map_id) {
-    let base_url = window.location.origin
-    url = base_url + url + map_id
+function get_map(button, url) {
+    let base_url = window.location.origin;
+    url = base_url + url;
     let temporary_input = document.createElement("input");
     temporary_input.value = url;
     document.body.appendChild(temporary_input);
     temporary_input.select();
     document.execCommand("copy");
     document.body.removeChild(temporary_input);
+
+    button.innerText = "Copied";
+    setTimeout(function(){ button.innerText="Foundry"; }, 500);
+
 }``
 
 
