794dc0cee27a8f86b4d5f1510f52c1eba95bf033
miki4920 <miki4920@gmail.com>
2022-03-04 21:45:32 +0000

    Integrated config with webscrapper

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
+import os
+
+from flask import Flask
+
+
 class CONFIG:
     MINIMUM_NAME_LENGTH = 3
-    MAXIMUM_NAME_LENGTH = 50
+    MAXIMUM_NAME_LENGTH = 128
+    SUBREDDITS = [
+        "battlemaps",
+        "dndmaps",
+        "dungeondraft",
+        "fantasymaps",
+        "miskasmaps",
+        "roll20",
+        "FoundryVTT"
+    ]
     IMAGE_EXTENSIONS = ["png", "jpg"]
     MINIMUM_IMAGE_SIZE_IN_BYTES = 5000
     MAXIMUM_IMAGE_SIZE_IN_BYTES = 20485760
@@ -11,3 +25,9 @@ class CONFIG:
     THUMBNAIL_SIZE = 512
     THUMBNAIL_DIRECTORY = UPLOAD_DIRECTORY + "/thumbnails"
     MAPS_PER_PAGE = 100
+
+
+app = Flask(__name__, static_url_path="/" + CONFIG.STATIC_DIRECTORY, static_folder=CONFIG.STATIC_DIRECTORY)
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f'mysql://{os.getenv("USERNAME")}:{os.getenv("PASSWORD")}@{os.getenv("IP")}/flask'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
