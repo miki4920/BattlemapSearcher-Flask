5f835d230e993ed58f8d7145ca7c26c6d94520db
miki4920 <miki4920@gmail.com>
2022-03-06 23:37:43 +0000

    Added more image functions

--- image.py
+++ image.py
@@ -1,11 +1,16 @@
 from PIL import Image
-from io import BytesIO
 
 
-def calculate_image_difference(image):
+from config import CONFIG
+
+
+Image.MAX_IMAGE_PIXELS = 182750400000
+
+
+def calculate_image_difference(path):
     resize_width = 9
     resize_height = 8
-    image = Image.open(BytesIO(image)).convert("LA")
+    image = Image.open(path).convert("LA")
     image = image.resize((resize_width, resize_height), Image.NEAREST)
     pixels = list(image.getdata())
     difference = []
@@ -17,8 +22,8 @@ def calculate_image_difference(image):
     return difference
 
 
-def hash_image(image):
-    difference = calculate_image_difference(image)
+def image_hash(path):
+    difference = calculate_image_difference(path)
     decimal_value = 0
     hash_string = ""
     for index, value in enumerate(difference):
@@ -29,3 +34,13 @@ def hash_image(image):
                 hex(decimal_value)[2:].rjust(2, "0"))
             decimal_value = 0
     return hash_string
+
+
+def image_thumbnail(path, thumbnail_path, extension):
+    with Image.open(path) as image:
+        thumbnail = image.resize((CONFIG.THUMBNAIL_SIZE, CONFIG.THUMBNAIL_SIZE), Image.ANTIALIAS)
+        thumbnail.save(thumbnail_path, "JPEG" if extension == "jpg" else extension)
+
+
+def image_dimensions(path):
+    return Image.open(path).size
