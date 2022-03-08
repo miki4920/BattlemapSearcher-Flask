d60d4dfd2660d1970b48b4524c707bd76c2d0aa8
miki4920 <miki4920@gmail.com>
2022-03-08 19:17:10 +0000

    Redone webscrapper so it uses the map creation function from model

--- webscrapper/web_scrapper.py
+++ webscrapper/web_scrapper.py
@@ -2,7 +2,7 @@ import json
 
 from io import BytesIO
 
-from image import image_hash
+from image import image_hash, image_thumbnail, image_dimensions
 from model import *
 from network import get_api_url, request_file
 from submission_handler import SubmissionHandler
@@ -10,16 +10,31 @@ from submission_handler import SubmissionHandler
 
 class WebScrapper(object):
     def get_submission(self, submission):
-        if SubmissionHandler.check_url(submission):
-            name = SubmissionHandler.get_name(submission)
-
-            submission = request_file(submission_dictionary["url"], timeout=1).content
-            submission_dictionary["width"], submission_dictionary["height"] = Image.open(BytesIO(submission)).size
-            submission_dictionary["hash"] = image_hash(submission)
-            if self.check_file_size(submission) and submission_dictionary["hash"] not in self.hash_set:
-                write_file(submission_dictionary["path"], submission)
-                self.submission_list.append(submission_dictionary)
-                self.hash_set.add(submission_dictionary["hash"])
+        name = submission["title"]
+        extension = submission["url"][-3:].lower()
+        print(submission["url"], extension)
+        image = request_file(submission["url"], timeout=1)
+        if image is None:
+            return
+        image = image.content
+        dimensions = image_dimensions(BytesIO(image))
+        if dimensions is None:
+            return
+        width, height = dimensions.size
+        if width < CONFIG.MINIMUM_IMAGE_DIMENSION and height < CONFIG.MINIMUM_IMAGE_DIMENSION:
+            return
+        square_width, square_height = SubmissionHandler.get_square_size(name)
+        battlemap_hash = image_hash(BytesIO(image))
+        name = SubmissionHandler.get_name(submission)
+        name = SubmissionHandler.get_path(name, extension)
+        path = name + "." + extension
+        if Map.query.filter_by(hash=battlemap_hash).first() is None and Map.query.filter_by(
+                path=CONFIG.UPLOAD_DIRECTORY + path).first() is None:
+            with open("../" + CONFIG.UPLOAD_DIRECTORY + path, "wb") as file:
+                file.write(image)
+            image_thumbnail("../" + CONFIG.UPLOAD_DIRECTORY + path, "../" + CONFIG.THUMBNAIL_DIRECTORY + path, extension)
+            create_map(name=name, extension=extension, path=path, width=width, height=height, square_width=square_width,
+                       square_height=square_height, hash=battlemap_hash)
 
     def scrapper(self):
         for subreddit in CONFIG.SUBREDDITS:
@@ -32,7 +47,8 @@ class WebScrapper(object):
                     if len(json_data) < 1:
                         break
                     for submission in json_data:
-                        print(submission)
+                        if SubmissionHandler.check_url(submission):
+                            self.get_submission(submission)
                     timestamp = int(json_data[-1]["created_utc"])
                 except json.decoder.JSONDecodeError:
                     timestamp += 1000
