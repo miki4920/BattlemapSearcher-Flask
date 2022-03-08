9476ea5b5a80729e2b4e69bdf3f0b731d6cc925c
miki4920 <miki4920@gmail.com>
2022-03-08 19:16:45 +0000

    Changed how submission handler deals with square sizes

--- webscrapper/submission_handler.py
+++ webscrapper/submission_handler.py
@@ -8,11 +8,11 @@ from model import BlackListHash
 class SubmissionHandler(object):
     @staticmethod
     def check_image_extension(url):
-        return url[-3:] not in CONFIG.IMAGE_EXTENSIONS
+        return url[-3:].lower() not in CONFIG.IMAGE_EXTENSIONS
 
     @staticmethod
     def check_image_origin(url):
-        return not re.search("i\..*\.it", url)
+        return not re.search("i\..*\.(jpg|png)", url)
 
     @staticmethod
     def check_url(submission):
@@ -32,18 +32,20 @@ class SubmissionHandler(object):
         return BlackListHash.filter_by(hash=submission_hash).first() is not None
 
     @staticmethod
-    def get_name(name):
+    def get_name(submission):
+        name = submission["title"]
         name = name.lower()
         name = re.sub(r"[^a-zA-Z_]", "_", name)
         name = re.sub(r"_+", "_", name)
         name = name.split("_")
-        name = filter(lambda word: len(word) >= 3 and word not in CONFIG.STOP_WORDS, name)
+        name = "_".join(filter(lambda word: len(word) >= 3 and word not in CONFIG.STOP_WORDS, name))
         name = re.sub(r"_+", "_", name)
         name = re.sub("_$|^_", "", name)
         while len(name) > CONFIG.MAXIMUM_NAME_LENGTH:
             name = "_".join(name.split("_")[:-1])
         if len(name) <= 3:
-            name = "review"
+            name = "Review"
+        name = " ".join([word.capitalize() for word in name.split("_")])
         return name
 
     @staticmethod
@@ -52,18 +54,17 @@ class SubmissionHandler(object):
 
     @staticmethod
     def get_path(name, extension):
-        unique_name = name
-        path = CONFIG.UPLOAD_DIRECTORY + unique_name + "." + extension
+        path = name
         count = 2
-        while os.path.exists(path):
-            unique_name = name + f"_{count}"
-            path = CONFIG.UPLOAD_DIRECTORY + unique_name + "." + extension
+        while os.path.exists("../" + CONFIG.UPLOAD_DIRECTORY + path + "." + extension):
+            path = name + f"_{count}"
             count += 1
-        return unique_name, path
+        return path
 
     @staticmethod
     def get_square_size(image_name):
-        image_name = re.sub("[^a-zA-Z0-9]", "", image_name)
+        image_name = image_name.lower()
+        image_name = re.sub("[^a-z0-9]", "", image_name)
         size = re.search("((?<!\d)\d{1,3}(?!\d))x((?<!\d)\d{1,3}(?!\d))", image_name)
         if not size:
             return None, None
