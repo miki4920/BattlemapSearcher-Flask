461834bcd672326d03f47dfdb1aea64079282bc9
miki4920 <miki4920@gmail.com>
2022-08-28 21:50:41 +0100

    Removed all of the submission handling from webscrapper as its functionality is now handled by Post

--- webscrapper/web_scrapper.py
+++ webscrapper/web_scrapper.py
@@ -1,58 +1,34 @@
-import json
+from sqlalchemy import desc
 
-from io import BytesIO
-
-from image import image_hash, image_thumbnail, image_dimensions
-from model import *
+from model import Map, CONFIG
 from network import get_api_url, request_file
-from submission_handler import SubmissionHandler
+from post import Post
 
 
-class WebScrapper(object):
-    def get_submission(self, submission):
-        name = submission["title"]
-        extension = submission["url"][-3:].lower()
-        image = request_file(submission["url"], timeout=1)
-        if image is None:
-            return
-        image = image.content
-        dimensions = image_dimensions(BytesIO(image))
-        if dimensions is None:
-            return
-        width, height = dimensions.size
-        if width < CONFIG.MINIMUM_IMAGE_DIMENSION and height < CONFIG.MINIMUM_IMAGE_DIMENSION:
-            return
-        square_width, square_height = SubmissionHandler.get_square_size(name)
-        battlemap_hash = image_hash(BytesIO(image))
-        name = SubmissionHandler.get_name(submission)
-        name = SubmissionHandler.get_path(name, extension)
-        path = name + "." + extension
-        if Map.query.filter_by(hash=battlemap_hash).first() is None and Map.query.filter_by(
-                path=CONFIG.UPLOAD_DIRECTORY + path).first() is None:
-            with open("../" + CONFIG.UPLOAD_DIRECTORY + path, "wb") as file:
-                file.write(image)
-            image_thumbnail("../" + CONFIG.UPLOAD_DIRECTORY + path, "../" + CONFIG.THUMBNAIL_DIRECTORY + path, extension)
-            create_map(name=name, extension=extension, path=path, width=width, height=height, square_width=square_width,
-                       square_height=square_height, hash=battlemap_hash)
+def get_timestamp():
+    newest_map = Map.query.order_by(desc(Map.timestamp)).first()
+    return newest_map.timestamp if newest_map is not None else 0
 
-    def scrapper(self):
-        for subreddit in CONFIG.SUBREDDITS:
-            timestamp = 0
-            while True:
-                url = get_api_url(subreddit, timestamp)
-                try:
-                    json_data = request_file(url)
-                    json_data = json_data.json()["data"]
-                    if len(json_data) < 1:
-                        break
-                    for submission in json_data:
-                        if SubmissionHandler.check_url(submission):
-                            self.get_submission(submission)
-                    timestamp = int(json_data[-1]["created_utc"])
-                except json.decoder.JSONDecodeError:
-                    timestamp += 1000
+
+def scrapper():
+    for subreddit in CONFIG.SUBREDDITS:
+        timestamp = get_timestamp()
+        while True:
+            url = get_api_url(subreddit, timestamp)
+            json_data = request_file(url)
+            if json_data is None:
+                timestamp += 1000
+                continue
+            json_data = json_data.json().get("data", [])
+            if len(json_data) < 1:
+                break
+            for post_data in json_data:
+                post = Post(post_data)
+                if post.valid:
+                    post.save()
+            timestamp = json_data[-1]["created_utc"]
 
 
 if __name__ == "__main__":
-    webscrapper = WebScrapper()
-    webscrapper.scrapper()
+    CONFIG.db.create_all()
+    scrapper()
