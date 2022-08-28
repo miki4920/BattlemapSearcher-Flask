9fccb1ae9ecf2cc442c81e879fa65180f32f2a58
miki4920 <miki4920@gmail.com>
2022-08-28 21:49:16 +0100

    Added accounting for non-200 status codes

--- webscrapper/network.py
+++ webscrapper/network.py
@@ -1,17 +1,17 @@
-import socket
-
 import requests
 import time
 
 
 def get_api_url(subreddit, timestamp):
-    return "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}" + f"&after={timestamp}&sort=asc&size=1000"
+    return "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}" + f"&after={timestamp}&sort=asc&size=500"
 
 
 def request_file(url, timeout=0):
     try:
         request = requests.get(url)
-    except Exception as e:
+        time.sleep(timeout)
+        if request.status_code == 200:
+            return request
         return None
-    time.sleep(timeout)
-    return request
+    except requests.exceptions.ConnectionError:
+        return None
