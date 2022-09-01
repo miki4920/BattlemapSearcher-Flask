d16c661e57ced527a4f483214b51a1303bcb2683
miki4920 <miki4920@gmail.com>
2022-09-01 01:19:47 +0100

    Overriden default pixel limit to prevent decompression errors

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -23,6 +24,7 @@ class CONFIG:
     THUMBNAIL_SIZE = 512
     THUMBNAIL_DIRECTORY = os.path.join(STATIC_DIRECTORY, "thumbnails")
     MAPS_PER_PAGE = 100
+    Image.MAX_IMAGE_PIXELS = 933120000
     app = Flask(__name__, static_url_path="/" + STATIC_DIRECTORY, static_folder=STATIC_DIRECTORY)
     app.config[
         'SQLALCHEMY_DATABASE_URI'] = f'mysql://{os.getenv("USERNAME")}:{os.getenv("PASSWORD")}@{os.getenv("IP")}/flask'
