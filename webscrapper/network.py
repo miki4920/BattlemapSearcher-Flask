c3ba6d6da603971d9a7ca786a746d98bbce21a64
miki4920 <miki4920@gmail.com>
2022-09-01 01:19:18 +0100

    Added network handling for exceptions

--- webscrapper/network.py
+++ webscrapper/network.py
@@ -1,6 +1,8 @@
 import requests
 import time
 
+import urllib3.exceptions
+
 
 def get_api_url(subreddit, timestamp):
     return "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}" + f"&after={timestamp}&sort=asc&size=500"
@@ -13,5 +15,5 @@ def request_file(url, timeout=0):
         if request.status_code == 200:
             return request
         return None
-    except requests.exceptions.ConnectionError:
+    except:
         return None
