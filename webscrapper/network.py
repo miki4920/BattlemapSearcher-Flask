d1178dc960ee4021b2534d49e5ae956d9b186b60
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-05-18 11:48:45 +0100

    Improved api error handling with API becoming more unstable

--- webscrapper/network.py
+++ webscrapper/network.py
@@ -1,19 +1,20 @@
-import requests
 import time
 
-import urllib3.exceptions
+import requests
 
 
 def get_api_url(subreddit, timestamp):
-    return "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}" + f"&after={timestamp}&sort=asc&size=500"
+    return "http://api.pushshift.io/reddit/search/submission/" + f"?subreddit={subreddit}" + f"&after={timestamp}&size=500&order=asc"
 
 
 def request_file(url, timeout=0):
     try:
-        request = requests.get(url)
+        request = requests.get(url, timeout=10)
         time.sleep(timeout)
         if request.status_code == 200:
             return request
         return None
-    except:
-        return None
+    except Exception as e:
+        print(url)
+        print("API does not work!")
+        quit()
