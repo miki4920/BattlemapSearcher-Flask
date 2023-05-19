d5b1373f08b6703db4a2ecbba4048cb3f4ac2d44
M Grobelny <366-mg448@users.noreply.campus.cs.le.ac.uk>
2023-05-19 21:30:41 +0100

    Updated exception handling to account for dead websites

--- webscrapper/network.py
+++ webscrapper/network.py
@@ -9,12 +9,13 @@ def get_api_url(subreddit, timestamp):
 
 def request_file(url, timeout=0):
     try:
-        request = requests.get(url, timeout=10)
+        request = requests.get(url)
         time.sleep(timeout)
         if request.status_code == 200:
             return request
         return None
+    except (requests.exceptions.InvalidURL, requests.exceptions.ConnectionError):
+        return None
     except Exception as e:
         print(url)
-        print("API does not work!")
-        quit()
+        raise e
